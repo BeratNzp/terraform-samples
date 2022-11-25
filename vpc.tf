0d8d5c22e6dd4a1930219f77d9f26df1d6959d7f
BeratNzp <beratnzp@gmail.com>
2022-11-26 00:09:07 +0300

    added something

--- vpc.tf
+++ vpc.tf
@@ -18,6 +18,11 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_route_table" "new_vpc_public_route_table" {
   vpc_id = aws_vpc.new_vpc.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
   tags = {
     Name = "new_vpc_public_route_table"
   }
