9a142a5430a99563692058fa690446fefc64f2f2
BeratNzp <beratnzp@gmail.com>
2022-11-25 23:56:53 +0300

    just try something

--- vpc.tf
+++ vpc.tf
@@ -38,14 +38,6 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_route_table" "new_vpc_public_route_table" {
   vpc_id = aws_vpc.new_vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-  route {
-    cidr_block = "10.0.0.0/16"
-  }
-
   tags = {
     Name = "new_vpc_public_route_table"
   }
